346580bb8d6a4af78ed982714749a5210c7082e7
lvxnull2 <184518908+lvxnull2@users.noreply.github.com>
2024-12-16 06:27:36 +0100

    [install] Simplify script

--- install.lua
+++ install.lua
@@ -1,11 +1,9 @@
 local URL = "https://raw.githubusercontent.com/lvxnull2/cc-storage/refs/heads/main/"
 
 local files = {
+  "lib/storage.lua",
   "mkcache.lua",
   "store.lua",
-  lib = {
-    "storage.lua"
-  },
 }
 
 local function download_file(url, path)
@@ -16,26 +14,8 @@ local function download_file(url, path)
   r.close()
 end
 
-local function recdl(dir)
-  local stack = {{prefix = "/", dir}}
-
-  while #stack > 0 do
-    local d = table.remove(stack)
-    local prefix = d.prefix
-    fs.makeDir(prefix)
-    d = d[1]
-
-    for k, v in pairs(d) do
-      if type(k) == "string" then
-        table.insert(stack, {prefix = fs.combine(prefix, k), v})
-      elseif type(k) == "number" then
-        local p = fs.combine(prefix, v)
-        print(("Downloading %s"):format(p))
-        download_file(URL .. p, p)
-      end
-    end
-  end
+for _, p in ipairs(files) do
+  print(("Downloading %s"):format(p))
+  download_file(URL .. p, p)
 end
-
-recdl(files)
 print("Download complete")
